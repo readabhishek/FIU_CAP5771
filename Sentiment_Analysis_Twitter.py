0d56c6261ff67d0b7735882fd2ff3e8c6b093595
readabhishek <38324150+readabhishek@users.noreply.github.com>
2018-04-21 15:51:48 -0400

    Update Sentiment_Analysis_Twitter.py

--- Sentiment_Analysis_Twitter.py
+++ Sentiment_Analysis_Twitter.py
@@ -1,4 +1,5 @@
 import re
+import json
 import tweepy
 from tweepy import OAuthHandler
 from textblob import TextBlob
@@ -12,10 +13,10 @@ class TwitterClient(object):
 
         # Initialize the keys and tokens for connection
         # keys and tokens from the Twitter Dev Console
-        tw_access_token = 'XXXXXXXXXXXX'
-        tw_access_token_secret = 'XXXXXXXXXXXXXX'
-        tw_consumer_key = 'XXXXXXXXXXXXX'
-        tw_consumer_secret = 'XXXXXXXXXXXXX'
+        tw_access_token = 'xxxxxxxxxxxxxxxxxxxxx'
+        tw_access_token_secret = 'xxxxxxxxxxxxxxxxxxx'
+        tw_consumer_key = 'xxxxxxxxxxxxxxxxxx'
+        tw_consumer_secret = 'xxxxxxxxxxxxxxxxx'
 
         # Authentication Process ......
         try:
@@ -32,7 +33,7 @@ class TwitterClient(object):
     def preProcess_tweet(self, tweet):
 
         # Utility function to clean tweet text by removing links, special characters using simple regex statements.
-
+        print(tweet)
         return ' '.join(
             re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])                                "
                    "| (\w +:\ / \ / \S +)", " ", tweet).split())
@@ -58,7 +59,7 @@ class TwitterClient(object):
         tweets = []
         try:
             # call twitter api to fetch tweets
-            fetched_tweets = self.twitter_api.search(q=query, count=count)
+            fetched_tweets = self.twitter_api.search(q=query, count=count, full_text='true')
             # print("Fetched Tweets ", fetched_tweets)
             # Loop and get each tweets...
             for tweet in fetched_tweets:
@@ -82,6 +83,13 @@ def print_sentiment_scores(sentence):
     snt = analyser.polarity_scores(sentence)
     print("{:-<40} {}".format(sentence, str(snt)))
 
+def load_JSON_file(filename, entity):
+    query = []; data = json.load(open(filename))
+    raw_keys = data[entity]["keys"].split(",")
+    title = data[entity]["title"].split(",")
+    for keys in raw_keys:
+        query.append(keys.strip(' \t\n\r'))
+    return query, title
 
 def plot_chart(ptweets, ntweets, tot_tweets, title):               # Plot the Pie-Chart
     labels = 'Positive Tweets', 'Negative Tweets', 'Neutral'
@@ -92,62 +100,86 @@ def plot_chart(ptweets, ntweets, tot_tweets, title):               # Plot the Pi
     plt.title(title); plt.axis('equal')
     plt.show()
 
+def parse_tweet_polarity(tweets):
+    tweets_polarity = [0, 1, 2]
+    tweets_polarity[0] = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
+    tweets_polarity[1] = [tweet for tweet in tweets if tweet['sentiment'] == 'negative']
+    tweets_polarity[2] = [tweet for tweet in tweets if tweet['sentiment'] == 'neutral']
+    return tweets_polarity
 
-def main():
-    # TwitterClient Object
-    twitter_api = TwitterClient()
-    # Get tweets
-    tweets_by_shortname = twitter_api.get_online_tweets(query='Amex', count=1000)
-    tweets_by_fullname = twitter_api.get_online_tweets(query='American Express', count=1000)
-    tweets_by_handle = twitter_api.get_online_tweets(query='@AmericanExpress', count=1000)
-    pos_tweets, neg_tweets, neu_tweet = [0, 1, 2], [0, 1, 2], [0, 1, 2]
-    pos_tweets[0] = [tweet for tweet in tweets_by_shortname if tweet['sentiment'] == 'positive']
-    pos_tweets[1] = [tweet for tweet in tweets_by_fullname if tweet['sentiment'] == 'positive']
-    pos_tweets[2] = [tweet for tweet in tweets_by_handle if tweet['sentiment'] == 'positive']
+def get_tweets_online(twitter_api, query, title):
+    positive_tweets, negative_tweets, neutral_tweets = [], [], []
 
-    neg_tweets[0] = [tweet for tweet in tweets_by_shortname if tweet['sentiment'] == 'negative']
-    neg_tweets[1] = [tweet for tweet in tweets_by_fullname if tweet['sentiment'] == 'negative']
-    neg_tweets[2] = [tweet for tweet in tweets_by_handle if tweet['sentiment'] == 'negative']
+    for key in query:
+        tweets_polarity = [0, 1, 2]
+        tweets = twitter_api.get_online_tweets(query=key, count=30)
+        tweets_polarity = parse_tweet_polarity(tweets)
+        for items in tweets_polarity[0]:
+            if items not in positive_tweets:
+                positive_tweets.append(items)
+        for items in tweets_polarity[1]:
+            if items not in negative_tweets:
+                negative_tweets.append(items)
+        for items in tweets_polarity[2]:
+            if items not in neutral_tweets:
+                neutral_tweets.append(items)
 
-    neu_tweet[0] = [tweet for tweet in tweets_by_handle if tweet['sentiment'] == 'neutral']
-
-    # print("Request: ", tweets)
-    # picking positive tweets from tweets
-    ptweets = pos_tweets[0] + pos_tweets[1] + pos_tweets[2]
-    tot_tweets = tweets_by_shortname + tweets_by_fullname + tweets_by_handle
+    tot_tweets = positive_tweets + negative_tweets + neutral_tweets
     # percentage of positive tweets
-    print("Positive tweets percentage: {} %".format(100 * len(ptweets) / len(tot_tweets)))
-    # picking negative tweets from tweets
-    ntweets = neg_tweets[0] + neg_tweets[1] + neg_tweets[2]
+    print("Positive tweets percentage: {} %".format(100 * len(positive_tweets) / len(tot_tweets)))
     # percentage of negative tweets
-    print("Negative tweets percentage: {} %".format(100 * len(ntweets) / len(tot_tweets)))
+    print("Negative tweets percentage: {} %".format(100 * len(negative_tweets) / len(tot_tweets)))
     # percentage of neutral tweets
-    # print("tweets: ", tweets_by_shortname); print("\n ntweets: ", ntweets); print("\n ptweets: ", ptweets)
-    print("Neutral tweets percentage: {} %".format(
-        100 * (len(tot_tweets) - len(ntweets) - len(ptweets)) / len(tot_tweets)))
+    print("Neutral tweets percentage: {} %".format(100 * (len(tot_tweets) - len(negative_tweets) - len(positive_tweets)) / len(tot_tweets)))
 
     # Plot the Graph...
-    plot_chart(ptweets, ntweets, tot_tweets, "Sentiment Analysis - American Express in Twitter")
-
-
+    plot_chart(positive_tweets, negative_tweets, tot_tweets, title)
 
     # printing first 5 positive tweets
     '''
     print("\n\nPositive tweets:")
-    for tweet in ptweets[:10]:
+    f = open("Twitter_Comments.txt", "w+")
+    for tweet in positive_tweets[:10]:
         print(tweet['text'])
+        f.write(tweet['text'])
+
 
     # printing first 5 negative tweets
     print("\n\nNegative tweets:")
-    for tweet in ntweets[:10]:
+    for tweet in negative_tweets[:10]:
         print(tweet['text'])
 
     # printing first 5 neutral tweets
     print("\n\n Neutral tweets:")
-    for tweet in neu_tweet[0][:10]:
+    for tweet in neutral_tweets[:10]:
         print(tweet['text'])
         print_sentiment_scores(tweet['text'])
+
     '''
+
+
+
+
+def main():
+    # TwitterClient Object
+    twitter_api = TwitterClient()
+
+    #Get keywords and official twitter handle for 'American Express' and use those keywords for retrieving the tweets.
+    query_Amex, title_Amex = load_JSON_file('Keywords.json', 'Amex')
+
+    # Get keywords and official twitter handle for 'Chase Bank' which is competitor for Amex and use those keywords for retrieving the tweets.
+    query_Chase, title_Chase = load_JSON_file('Keywords.json', 'Chase')
+
+    # Get keywords and official twitter handle for 'Capital One Bank' which is competitor for Amex and use those keywords for retrieving the tweets.
+    query_CapOne, title_CapOne = load_JSON_file('Keywords.json', 'CapitalOne')
+
+    get_tweets_online(twitter_api, query_Amex, *title_Amex)      # Astricks(*) is used to unpack the tuple value.
+    get_tweets_online(twitter_api, query_Chase, *title_Chase)
+    get_tweets_online(twitter_api, query_CapOne, *title_CapOne)
+
+
+
+
 
 
 if __name__ == "__main__":
