d16598497bcaf5e7d98cb2475501706a61ef3e3b
readabhishek <38324150+readabhishek@users.noreply.github.com>
2018-04-27 20:34:05 -0400

    Update Input_Command_Prompt.py

--- Input_Command_Prompt.py
+++ Input_Command_Prompt.py
@@ -1,68 +1,109 @@
 import datetime
+from colorama import init, Fore, Back, Style
+from termcolor import colored
 from CAP5771.Project import Load_Tweets_Batch
 from CAP5771.Project import Sentiment_Analysis_Twitter
 from CAP5771.Project import Trend_Analysis_Tweets
+from CAP5771.Project import Get_Company_Configuration
 
 
+# Initializing the variables.
 operation = '0'
+now = datetime.datetime.now()
+from_date = now - (datetime.timedelta(days=1))
+to_date, from_date = str(now.date()), str(from_date.date())
+companyList, count = Get_Company_Configuration.getCompanyDetailsList()    # Read from configuration file and get company names and number dynamically.
+
 
 while (operation != 'E'):
+    #print(Style.BRIGHT + Fore.BLUE);print(Style.RESET_ALL)
     print("\n\nSelect options from below menu: (To exit, type E)-------- ")
-    print("Select an operation: (1) Analyze sentiment  (2) Load tweets from yesterday  (3) Watch the sentiments trend (4) Purge old data.")
+    if (operation == 'E'):
+        print("Exiting the Menu. Bye...")
+        break
+
+    print("Select an Operation: (1) Twitter: Analyze Sentiments  (2) Load Tweets (On-demand)  (3) Sentiment Trends (4) Purge Old data/tweets.")
     operation = input("To select, type  1  2  3  or 4 : \n")
-    print("Select company for the operation: (1) American Express  (2) Chase   (3) Capital One. ")
-    company = input("To select, type 1  2 or 3 :\n")
 
-    now = datetime.datetime.now()
-    from_date = now - (datetime.timedelta(days=1))
-    to_date, from_date = str(now.date()), str(from_date.date())
+    print("Select company for the operation:")         # Loop and update company name at run time.
+    company_choice = ""
+    for n in range(count):
+        company_choice = company_choice + str(n+1) + ") " + companyList[n+1]["name"] + "   "
+    company_choice = company_choice + str(count+2) + ") " + "All of them  "
+    print(company_choice)
+    company = input("Select your options, enter the number (1  2  3 so on):\n")  # Select options for company name
+    company = int(company)  # Convert this to compare with number
 
-    if (operation == '4'):
-        print("Select (1) To purge yesterday/'s data  (2) To purge data for custom date")
+    # For (1) Twitter: Analyze Sentiments:
+    if  operation == '1' and company < (count+2):
+        filename, title = companyList[int(company)]["twitter_data_file"], companyList[int(company)]["sentiment_title"]
+        Sentiment_Analysis_Twitter.main(filename=filename, title=title)
+    if  operation == '1' and company == (count+2):
+        for n in range(count):
+            filename, title = companyList[n+1]["twitter_data_file"], companyList[n+1]["sentiment_title"]
+            Sentiment_Analysis_Twitter.main(filename=filename, title=title)
+
+
+    # For (2) Load Tweets (On-demand)
+    if  operation == '2':
+        print("Select (1) To load today's tweets  (2) To load tweets from custom date")
+        load_option = input("To select, type  1  or 2 : \n")
+        if (load_option == '2'):
+            from_date = input("Load tweets: Input, From date (in YYYY-MM-DD): \n")
+            to_date = input("Load tweets: Input, To date   (in YYYY-MM-DD): \n")
+
+        if company < (count+2):
+            filename, query = companyList[int(company)]["twitter_data_file"], companyList[int(company)]["keys"]
+            Load_Tweets_Batch.main(operation='2', query=query, filename=filename, start_date=to_date, since_date=from_date)
+        else:
+            for n in range(count):
+                filename, query = companyList[n+1]["twitter_data_file"], companyList[n+1]["keys"]
+                Load_Tweets_Batch.main(operation='2', query=query, filename=filename, start_date=to_date, since_date=from_date)
+
+
+    # For (3) Sentiment Trends
+
+    if operation == '3':
+        print("Select (1) See trends from last 24 hrs  (2) See trends from custom date")
+        trend_option = input("To select, type  1  or 2 : \n")
+        twitter_data_file, twitter_trend_file, title = companyList[int(company)]["twitter_data_file"], companyList[int(company)]["twitter_trend_file"], companyList[int(company)]["trend_title"]
+        if (trend_option == '2'):
+            from_date = input("Sentiments Trend: Input, From date (in YYYY-MM-DD): \n")
+            to_date = input("Sentiments Trend: Input, To date   (in YYYY-MM-DD): \n")
+
+        if company < (count + 2):
+            Trend_Analysis_Tweets.sort_data_by_dates(twitter_data_file)
+            Trend_Analysis_Tweets.collect_data_for_trendReporting(twitter_data_file, twitter_trend_file, from_date, to_date)
+            Trend_Analysis_Tweets.display_trends(twitter_data_file, twitter_trend_file, title)
+        else:
+            for n in range(count):
+                twitter_data_file, twitter_trend_file, title = companyList[n+1]["twitter_data_file"], companyList[n+1]["twitter_trend_file"], companyList[n+1]["trend_title"]
+                Trend_Analysis_Tweets.sort_data_by_dates(twitter_data_file)
+                Trend_Analysis_Tweets.collect_data_for_trendReporting(twitter_data_file, twitter_trend_file, from_date, to_date)
+                Trend_Analysis_Tweets.display_trends(twitter_data_file, twitter_trend_file, title)
+
+
+    # For (4) Purge Old data/tweets.
+
+    if operation == '4':
+        print("Select (1) Purge data from last 24 hrs  (2) Purge data from custom date")
         purge_option = input("To select, type  1  or 2 : \n")
         if (purge_option == '2'):
-            from_date = input("Data purge: Input, From date : \n")
-            to_date = input("Data purge: Input, To date : \n")
-            # Here call - Purge function.
+            from_date = input("Purge data: Input, From date (in YYYY-MM-DD): \n")
+            to_date = input("Purge data: Input, To date   (in YYYY-MM-DD): \n")
+            twitter_data_file = companyList[int(company)]["twitter_data_file"]
+
+        if company < (count + 2):
+            Load_Tweets_Batch.main(operation='4', filename=twitter_data_file, purge_date_from=from_date, purge_date_to=to_date)
+        else:
+            for n in range(count):
+                twitter_data_file = companyList[n+1]["twitter_data_file"]
+                Load_Tweets_Batch.main(operation='4', filename=twitter_data_file, purge_date_from=from_date, purge_date_to=to_date)
 
 
 
-    if (operation == 'E'):
-        print("Ok no update.. Exiting the program. Bye...")
-
-    elif (operation == '1' and company == '1'):
-        Sentiment_Analysis_Twitter.main(filename='Tweets_Amex.csv', title='Sentiments Analysis in Twitter - American Express')
-    elif (operation == '1' and company == '2'):
-        Sentiment_Analysis_Twitter.main(filename='Tweets_Chase.csv', title='Sentiments Analysis in Twitter - Chase Bank')
-    elif (operation == '1' and company == '3'):
-        Sentiment_Analysis_Twitter.main(filename='Tweets_CapOne.csv', title='Sentiments Analysis in Twitter - Capital One Bank')
 
 
-    elif (operation == '2' and company == '1'):
-        Load_Tweets_Batch.main(operation ='2', query='@AmericanExpress', filename='Tweets_Amex.csv', start_date=to_date, since_date=from_date)
-    elif (operation == '2' and company == '2'):
-        Load_Tweets_Batch.main(operation='B', query='@Chase', filename='Tweets_Chase.csv', start_date=to_date, since_date=from_date)
-    elif (operation == '2' and company == '3'):
-        Load_Tweets_Batch.main(operation = '2', query='@CapitalOne', filename='Tweets_CapOne.csv', start_date=to_date, since_date=from_date)
 
-    elif (operation == '3' and company == '1'):
-        Trend_Analysis_Tweets.sort_data_by_dates("Tweets_Amex.csv");
-        Trend_Analysis_Tweets.collect_data_for_trendReporting("Tweets_Amex.csv", "Tweets_Amex_for_Trend.csv", from_date, to_date)
-        Trend_Analysis_Tweets.display_trends("Tweets_Amex.csv", "Tweets_Amex_for_Trend.csv")
 
-    elif (operation == '3' and company == '2'):
-        Trend_Analysis_Tweets.sort_data_by_dates("Tweets_Chase.csv");
-        Trend_Analysis_Tweets.collect_data_for_trendReporting("Tweets_Chase.csv", "Tweets_Chase_for_Trend.csv", from_date, to_date)
-        Trend_Analysis_Tweets.display_trends("Tweets_Chase.csv", "Tweets_Chase_for_Trend.csv")
 
-    elif (operation == '3' and company == '3'):
-        Trend_Analysis_Tweets.sort_data_by_dates("Tweets_CapOne.csv");
-        Trend_Analysis_Tweets.collect_data_for_trendReporting("Tweets_CapOne.csv", "Tweets_CapOne_for_Trend.csv", from_date, to_date)
-        Trend_Analysis_Tweets.display_trends("Tweets_CapOne.csv", "Tweets_CapOne_for_Trend.csv")
-
-    elif (operation == '4' and company == '1'):
-        Load_Tweets_Batch.main(operation = '4',query='@AmericanExpress', filename='Tweets_Amex.csv', purge_date_from = from_date, purge_date_to = to_date)
-    elif (operation == '4' and company == '2'):
-        Load_Tweets_Batch.main(operation = '4',query='@Chase', filename='Tweets_Chase.csv', purge_date_from = from_date, purge_date_to = to_date)
-    elif (operation == '4' and company == '3'):
-        Load_Tweets_Batch.main(operation = '4',query='@CapitalOne', filename='Tweets_CapOne.csv', purge_date_from = from_date, purge_date_to = to_date)
